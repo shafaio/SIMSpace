5a4294efe13c235ab23fb994451f3971df2b3b32
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2022-08-09 14:54:49 +0800

    add jabatan atribute

--- resources/views/admin/tambahData.blade.php
+++ resources/views/admin/tambahData.blade.php
@@ -25,9 +25,18 @@
                         <input type="text" class="form-control radius-none" id="nama_anggota" name="nama_anggota" placeholder="Nama Pegawai" style="font-size: 14px; height: 48px;">
                     </div>
                     <h6 class="p-3 d-block mb-3" style="width: 100%; border-radius: 0; background-color: #05323C ; color: white !important; font-weight: 600;">Jabatan </h6>
-                    <div class="mb-3">
+                    <select class="form-select" aria-label="Default select example" style="font-size: 14px; height: 48px;">
+                        <option >Pilih Jabatan</option>
+                        <option value="1">Chief Executive Officer</option>
+                        <option value="2">Chief Marketing Officer</option>
+                        <option value="3">Chief Operational Officer</option>
+                        <option value="4">Chief Technology Officer</option>
+                        <option value="5">Chief Finance Officer</option>
+                        <option value="6">Chief Human Resource Officer</option>
+                    </select>
+                    <!-- <div class="mb-3">
                         <input type="text" class="form-control radius-none" id="jabatan" name="jabatan" placeholder="Jabatan" style="font-size: 14px; height: 48px;">
-                    </div>
+                    </div> -->
                     <h6 class="p-3 d-block mb-3 mt-4" style="width: 100%; border-radius: 0; background-color: #05323C ; color: white !important; font-weight: 600;">Password</h6>
                     <div class="mb-3">
                         <input type="password" class="form-control radius-none" id="password" name="password" placeholder="Password" style="font-size: 14px; height: 48px;">
